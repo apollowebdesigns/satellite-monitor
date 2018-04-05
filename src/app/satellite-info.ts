135f698406785ae85ad01c229fc048d811e89d68
Atkins, Leona <leona.atkins@metoffice.gov.uk>
2018-04-05 12:51:09 +0100

    Update layout add class and constants.

--- src/app/satellite-info.ts
+++ src/app/satellite-info.ts
@@ -1,11 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Crown copyright 2018, Met Office. All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////
-import { SatelliteInfo } from './SatelliteInfo';
+import { Satellite } from './satellite';
 
-export const SATINFO: SatInfo[] = [
-    { id: 1, name: 'Satellite 1', details: '', lastConnected:''},
-    { id: 2, name: 'Satellite 2', details: '', lastConnected:''},
-    { id: 3, name: 'Satellite 3', details: '', lastConnected:''},
-    { id: 4, name: 'Satellite 4', details: '', lastConnected:''},
+export const SATINFO: Satellite[] = [
+    { id: 1, name: 'Satellite 1', details: '', lastConnect:'', status:'OK'},
+    { id: 2, name: 'Satellite 2', details: '', lastConnect:'', status:'OK'},
+    { id: 3, name: 'Satellite 3', details: '', lastConnect:'', status:'OK'},
+    { id: 4, name: 'Satellite 4', details: '', lastConnect:'', status:'OK'}
 ];
