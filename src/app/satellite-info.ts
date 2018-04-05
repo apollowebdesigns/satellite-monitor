3e81aa5fb4771519353aea4b2f9e258d6e8ee3e9
Atkins, Leona <leona.atkins@metoffice.gov.uk>
2018-04-05 10:20:14 +0100

    Create layout and mock data

---
+++ src/app/satellite-info.ts
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// Crown copyright 2018, Met Office. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+import { SatelliteInfo } from './SatelliteInfo';
+
+export const SATINFO: SatInfo[] = [
+    { id: 1, name: 'Satellite 1', details: '', lastConnected:''},
+    { id: 2, name: 'Satellite 2', details: '', lastConnected:''},
+    { id: 3, name: 'Satellite 3', details: '', lastConnected:''},
+    { id: 4, name: 'Satellite 4', details: '', lastConnected:''},
+];
