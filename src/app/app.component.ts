f95910a0d2e6b3d844704a986e307567ca8b4db0
Atkins, Leona <leona.atkins@metoffice.gov.uk>
2018-04-06 15:41:37 +0100

    style, a little less 1999?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements  OnInit {
   constructor (private changeDetector: ChangeDetectorRef) {}
 
   private EVENT_URL = 'http://localhost:8080/stream';
-  private message: string = '0';
+  private message: Array = [];
 
   title = 'Satellite Monitor';
   satInfo = SATINFO;
@@ -31,26 +31,30 @@ export class AppComponent implements  OnInit {
 
     // listing to server messages
     this.ws.onmessage = (evt) => {
-      this.logServerMessage(evt.data);
+      console.log(evt.data);
+      if (evt.data !== null) {
 
-      // update the model
-      this.message = Array.from(JSON.parse(evt.data));
-      //console.log(this.message);
-      this.message.forEach(item => {
-        console.log(item);
-        let sat = this.satInfo.find(x => x.id == item.id);
-        //console.log(sat);
-        sat.status = item.state;
-        if (sat.status === 'OK') {
-          sat.lastConnect = new Date().toISOString();
-        }
-        //this.satInfo[item.id].status = item.state;
-        //console.log(this.satInfo);
-      });
-
+        this.logServerMessage(evt.data);
+        // update the model
+        this.message = Array.from(JSON.parse(evt.data));
+        //console.log(this.message);
+        this.message.forEach(item => {
+          console.log(item);
+          let sat = this.satInfo.find(x => x.id == item.id);
+          //console.log(sat);
+          sat.status = item.state;
+          if (sat.status === 'OK') {
+            sat.lastConnect = new Date().toISOString();
+          }
+          //this.satInfo[item.id].status = item.state;
+          //console.log(this.satInfo);
+        });
+      }
+      this.message = ["No Data"];
       // manually detect changes
       this.changeDetector.detectChanges();
     };
+
   }
 
   logServerMessage (data) {
