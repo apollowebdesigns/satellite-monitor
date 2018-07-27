17b7525f4809373b212128913f4eb675a43e58d2
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-07-27 12:15:54 +0100

    changed url to talk to python web socket server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,8 @@ export class AppComponent implements  OnInit {
 
   ngOnInit() {
     //let eventSource = window['EventSource'];
-    this.ws = new EventSource(this.EVENT_URL, { withCredentials: true });
+    // this.ws = new EventSource(this.EVENT_URL, { withCredentials: true });
+    this.ws = new WebSocket('ws://127.0.0.1:5678/');
 
     // listing to server messages
     this.ws.onmessage = (evt) => {
